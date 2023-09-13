9909e8fc238d2b45c5d42f10b1e1c1da53fd9066
danytronyco12 <pruebacol52@hotmail.com>
2023-09-12 20:29:18 -0400

    okok

--- resources/js/Pages/contactenos.vue
+++ resources/js/Pages/contactenos.vue
@@ -22,6 +22,7 @@
             <div
               class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300"
             >
+                
               <p class="mx-4 mb-0 text-center text-2xl font-semibold dark:text-white">
                 Contactanos
               </p>
